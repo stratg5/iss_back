0f8f0a629a6bdc3e378e8296aa1f3f7e6f26ce6c
aaronrood <aaron.s.rood@gmail.com>
2022-04-07 17:26:50 -0400

    call iss location api

--- main.rb
+++ main.rb
@@ -1,4 +1,21 @@
+require 'socket'
+require 'uri'
+require 'net/http'
+require 'json'
 
-# set up server with endpoint
-# call ISS API to fetch location
-# call endpoint from the client
+server = TCPServer.new 5678
+ 
+while session = server.accept
+  request = session.gets
+
+  uri = URI('http://api.open-notify.org/iss-now.json')
+  res = Net::HTTP.get_response(uri)
+  res_hash = JSON.parse(res.body)
+ 
+  session.print "HTTP/1.1 200\r\n" # 1
+  session.print "Content-Type: text/html\r\n" # 2
+  session.print "\r\n" # 3
+  session.print res_hash["iss_position"]
+ 
+  session.close
+end
